1f0961ae0193bca7f6ab777ce699209adf335e22
Marcel <15875473+melcar@users.noreply.github.com>
2024-06-17 17:18:29 +0200

    fix fugitive config + setup tagbar

---
+++ nvim/after/plugin/tagbar.lua
@@ -0,0 +1,7 @@
+
+vim.g.tagbar_autoclose =1
+vim.g.tagbar_autofocus = 1
+vim.g.tagbar_show_tag_count = 1
+vim.g.tagbar_show_linenumbers=2
+
+vim.keymap.set("n", "<leader>t", vim.cmd.TagbarOpen)
--- nvim/lua/melcar/plugins/fugitive.lua
+++ nvim/lua/melcar/plugins/fugitive.lua
@@ -1,5 +1,4 @@
 return {
     "tpope/vim-fugitive",
-    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
 }
