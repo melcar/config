a255355d9e530485f9cb3e54aff03566f58fbec1
melcar <15875473+melcar@users.noreply.github.com>
2024-06-21 18:30:17 +0200

    added  bench and doc generation shortcut

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,3 +1,5 @@
 -- run test in rust file
 vim.keymap.set("n" ,"<leader>c",function() vim.cmd(":!cargo test %:t:r") end)
 vim.keymap.set("n" ,"<leader>r",function() vim.cmd(":!cargo run %:t:r")end)
+vim.keymap.set("n" ,"<leader>d",function() vim.cmd(":!cargo doc --no-deps")end)
+vim.keymap.set("n" ,"<leader>b",function() vim.cmd(":!cargo bench --features remote-benchmarks --bench huffman")end)
