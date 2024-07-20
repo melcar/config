fc48927de3e19c21d5979c935d09dc4882d66c73
melcar <15875473+melcar@users.noreply.github.com>
2024-07-20 13:50:28 +0200

    added remap and plugins

---
+++ nvim/after/plugin/neogen.lua
@@ -0,0 +1,4 @@
+require('neogen').generate()
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
---
+++ nvim/lua/melcar/plugins/lazydoc.lua
@@ -0,0 +1,10 @@
+return{
+  "jla2000/lazydocs.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "nvimtools/none-ls.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {},
+}
